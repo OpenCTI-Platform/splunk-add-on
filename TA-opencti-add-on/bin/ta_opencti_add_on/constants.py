eb15f0118dc9923aebccf7f915c7b64885b36793
Romain Guignard <romain.guignard@filigran.io>
2024-12-23 23:32:21 +0100

    https://github.com/OpenCTI-Platform/splunk-add-on/issues/4

--- TA-opencti-add-on/bin/ta_opencti_add_on/constants.py
+++ TA-opencti-add-on/bin/ta_opencti_add_on/constants.py
@@ -1,3 +1,3 @@
-CONNECTOR_ID = "a6edc906-2f9f-5fb2-a373-efac406f0ef2" # hard-coded opencti connector Identifier
+CONNECTOR_ID = "a6edc906-2f9f-5fb2-a373-efac406f0ef2"  # hard-coded opencti connector Identifier
 CONNECTOR_NAME = "Splunk App"  # hard-coded opencti connector name
-VERIFY_SSL = True # SSL verification by default
+VERIFY_SSL = True  # SSL verification by default
