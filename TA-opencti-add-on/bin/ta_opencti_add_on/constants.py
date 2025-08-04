ed2c65a9cd2decf72b287c12c74bef96168401ca
Jason Maurath <jason.maurath@filigran.io>
2025-08-04 11:23:11 -0500

    Changed default lookup to opencti_lookup

--- TA-opencti-add-on/bin/ta_opencti_add_on/constants.py
+++ TA-opencti-add-on/bin/ta_opencti_add_on/constants.py
@@ -1,5 +1,6 @@
-CONNECTOR_ID = "a6edc906-2f9f-5fb2-a373-efac406f0ef2"  # hard-coded opencti connector Identifier
+CONNECTOR_ID = (
+    "a6edc906-2f9f-5fb2-a373-efac406f0ef2"  # hard-coded opencti connector Identifier
+)
 CONNECTOR_NAME = "Splunk App"  # hard-coded opencti connector name
 VERIFY_SSL = True  # SSL verification by default
-INDICATORS_KVSTORE_NAME = "opencti_indicators"
-
+INDICATORS_KVSTORE_NAME = "opencti_lookup"
